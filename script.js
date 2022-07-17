b4f28098a36403846d0d50155314dbc9dfea6d69
cem-sucu <cems.cems.17@gmail.com>
2022-07-17 09:14:47 +0200

    css change

--- script.js
+++ script.js
@@ -1,57 +1,67 @@
-window.onload = function(){
-    var canvasWidth = 900;
-    var canvasHeight = 600;
-    var blockSize = 30;
+window.onload = function () {
+    var canvasWidth = 800;
+    var canvasHeight = 500;
+    var blockSize = 25;
     var ctx;
     var delay = 100;
     var xCoord = 0;
     var yCoord = 0;
     var snakee;
-    var applee; 
-    var widthInBlocks = canvasWidth/blockSize;
-    var heightInBlocks = canvasHeight/blockSize;
+    var applee;
+    var widthInBlocks = canvasWidth / blockSize;
+    var heightInBlocks = canvasHeight / blockSize;
     var score;
     var timeOut;
-    
+
     init();
-    
-    function init(){
-        var canvas = document.createElement('canvas');
+
+    function init() {
+        var canvas = document.createElement("canvas");
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
-        canvas.style.border = "30px solid gray";
+        canvas.style.border = "30px solid crimson";
+        canvas.style.borderRadius = "50px";
         canvas.style.margin = "50px auto";
         canvas.style.display = "block";
         canvas.style.backgroundColor = "#ddd";
         document.body.appendChild(canvas);
-        ctx = canvas.getContext('2d');
-        snakee = new Snake([[6,4],[5,4],[4,4],[3,4],[2,4]],"right");
-        applee = new Apple([10,10]);
+        ctx = canvas.getContext("2d");
+        snakee = new Snake(
+            [
+                [6, 4],
+                [5, 4],
+                [4, 4],
+                [3, 4],
+                [2, 4],
+            ],
+            "right"
+        );
+        applee = new Apple([10, 10]);
         score = 0;
         refreshCanvas();
     }
-    
-    function refreshCanvas(){
+
+    function refreshCanvas() {
         snakee.advance();
-        if (snakee.checkCollision()){
+        if (snakee.checkCollision()) {
             gameOver();
         } else {
-            if (snakee.isEatingApple(applee)){
+            if (snakee.isEatingApple(applee)) {
                 score++;
                 snakee.ateApple = true;
                 do {
-                    applee.setNewPosition(); 
-                } while(applee.isOnSnake(snakee));
+                    applee.setNewPosition();
+                } while (applee.isOnSnake(snakee));
             }
-            ctx.clearRect(0,0,canvasWidth,canvasHeight);
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
             drawScore();
             snakee.draw();
             applee.draw();
-            timeOut = setTimeout(refreshCanvas,delay);
-         }
+            timeOut = setTimeout(refreshCanvas, delay);
+        }
     }
-    
-    function gameOver(){
+
+    function gameOver() {
         ctx.save();
         ctx.font = "bold 70px sans-serif";
         ctx.fillStyle = "#000";
@@ -64,20 +74,37 @@ window.onload = function(){
         ctx.strokeText("T'AS PERDU", centreX, centreY - 180);
         ctx.fillText("T'AS PERDU", centreX, centreY - 180);
         ctx.font = "bold 30px sans-serif";
-        ctx.strokeText("Appuie sur la touche Espace pour rejouer", centreX, centreY - 120);
-        ctx.fillText("Appuie sur la touche Espace pour rejouer", centreX, centreY - 120);
+        ctx.strokeText(
+            "Appuie sur la touche Espace pour rejouer",
+            centreX,
+            centreY - 120
+        );
+        ctx.fillText(
+            "Appuie sur la touche Espace pour rejouer",
+            centreX,
+            centreY - 120
+        );
         ctx.restore();
     }
-    
-    function restart(){
-        snakee = new Snake([[6,4],[5,4],[4,4],[3,4],[2,4]],"right");
-        applee = new Apple([10,10]);
+
+    function restart() {
+        snakee = new Snake(
+            [
+                [6, 4],
+                [5, 4],
+                [4, 4],
+                [3, 4],
+                [2, 4],
+            ],
+            "right"
+        );
+        applee = new Apple([10, 10]);
         score = 0;
         clearTimeout(timeOut);
         refreshCanvas();
     }
-    
-    function drawScore(){
+
+    function drawScore() {
         ctx.save();
         ctx.font = "bold 200px sans-serif";
         ctx.fillStyle = "gray";
@@ -88,30 +115,30 @@ window.onload = function(){
         ctx.fillText(score.toString(), centreX, centreY);
         ctx.restore();
     }
-    
-    function drawBlock(ctx, position){
-        var x = position[0]*blockSize;
-        var y = position[1]*blockSize;
-        ctx.fillRect(x,y,blockSize,blockSize);
+
+    function drawBlock(ctx, position) {
+        var x = position[0] * blockSize;
+        var y = position[1] * blockSize;
+        ctx.fillRect(x, y, blockSize, blockSize);
     }
-    
-    function Snake(body, direction){
+
+    function Snake(body, direction) {
         this.body = body;
         this.direction = direction;
         this.ateApple = false;
-        
-        this.draw = function(){
+
+        this.draw = function () {
             ctx.save();
-            ctx.fillStyle="#ff0000";
-            for (var i=0 ; i < this.body.length ; i++){
-                drawBlock(ctx,this.body[i]);
+            ctx.fillStyle = "#483d8b";
+            for (var i = 0; i < this.body.length; i++) {
+                drawBlock(ctx, this.body[i]);
             }
             ctx.restore();
         };
-        
-        this.advance = function(){
+
+        this.advance = function () {
             var nextPosition = this.body[0].slice();
-            switch(this.direction){
+            switch (this.direction) {
                 case "left":
                     nextPosition[0] -= 1;
                     break;
@@ -125,35 +152,33 @@ window.onload = function(){
                     nextPosition[1] -= 1;
                     break;
                 default:
-                    throw("invalid direction");
+                    throw "invalid direction";
             }
             this.body.unshift(nextPosition);
-            if (!this.ateApple)
-                this.body.pop();
-            else
-                this.ateApple = false;
+            if (!this.ateApple) this.body.pop();
+            else this.ateApple = false;
         };
-        
-        this.setDirection = function(newDirection){
+
+        this.setDirection = function (newDirection) {
             var allowedDirections;
-            switch(this.direction){
+            switch (this.direction) {
                 case "left":
                 case "right":
-                    allowedDirections=["up","down"];
+                    allowedDirections = ["up", "down"];
                     break;
                 case "down":
                 case "up":
-                    allowedDirections=["left","right"];
-                    break;  
-               default:
-                    throw("invalid direction");
+                    allowedDirections = ["left", "right"];
+                    break;
+                default:
+                    throw "invalid direction";
             }
-            if (allowedDirections.indexOf(newDirection) > -1){
+            if (allowedDirections.indexOf(newDirection) > -1) {
                 this.direction = newDirection;
             }
         };
-        
-        this.checkCollision = function(){
+
+        this.checkCollision = function () {
             var wallCollision = false;
             var snakeCollision = false;
             var head = this.body[0];
@@ -166,65 +191,68 @@ window.onload = function(){
             var maxY = heightInBlocks - 1;
             var isNotBetweenHorizontalWalls = snakeX < minX || snakeX > maxX;
             var isNotBetweenVerticalWalls = snakeY < minY || snakeY > maxY;
-            
+
             if (isNotBetweenHorizontalWalls || isNotBetweenVerticalWalls)
                 wallCollision = true;
-            
-            for (var i=0 ; i<rest.length ; i++){
+
+            for (var i = 0; i < rest.length; i++) {
                 if (snakeX === rest[i][0] && snakeY === rest[i][1])
                     snakeCollision = true;
             }
-            
-            return wallCollision || snakeCollision;        
+
+            return wallCollision || snakeCollision;
         };
-        
-        this.isEatingApple = function(appleToEat){
+
+        this.isEatingApple = function (appleToEat) {
             var head = this.body[0];
-            if (head[0] === appleToEat.position[0] && head[1] === appleToEat.position[1])
+            if (
+                head[0] === appleToEat.position[0] &&
+                head[1] === appleToEat.position[1]
+            )
                 return true;
-            else
-                return false;
-        }
-        
-    }
-    
-    function Apple(position){
-        this.position = position;
-        
-        this.draw = function(){
-          ctx.save();
-          ctx.fillStyle = "#33cc33";
-          ctx.beginPath();
-          var radius = blockSize/2;
-          var x = this.position[0]*blockSize + radius;
-          var y = this.position[1]*blockSize + radius;
-          ctx.arc(x, y, radius, 0, Math.PI*2, true);
-          ctx.fill();
-          ctx.restore();
+            else return false;
         };
-        
-        this.setNewPosition = function(){
-            var newX = Math.round(Math.random()*(widthInBlocks-1));
-            var newY = Math.round(Math.random()*(heightInBlocks-1));
-            this.position = [newX,newY];
-        }; 
-        
-        this.isOnSnake = function(snakeToCheck){
+    }
+
+    function Apple(position) {
+        this.position = position;
+
+        this.draw = function () {
+            ctx.save();
+            ctx.fillStyle = "#33cc33";
+            ctx.beginPath();
+            var radius = blockSize / 2;
+            var x = this.position[0] * blockSize + radius;
+            var y = this.position[1] * blockSize + radius;
+            ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+            ctx.fill();
+            ctx.restore();
+        };
+
+        this.setNewPosition = function () {
+            var newX = Math.round(Math.random() * (widthInBlocks - 1));
+            var newY = Math.round(Math.random() * (heightInBlocks - 1));
+            this.position = [newX, newY];
+        };
+
+        this.isOnSnake = function (snakeToCheck) {
             var isOnSnake = false;
-            for (var i=0 ; i < snakeToCheck.body.length ; i++){
-                if(this.position[0] === snakeToCheck.body[i][0] && this.position[1] === snakeToCheck.body[i][1]){
-                    isOnSnake = true;     
+            for (var i = 0; i < snakeToCheck.body.length; i++) {
+                if (
+                    this.position[0] === snakeToCheck.body[i][0] &&
+                    this.position[1] === snakeToCheck.body[i][1]
+                ) {
+                    isOnSnake = true;
                 }
             }
             return isOnSnake;
         };
-
     }
-    
-    document.onkeydown = function handleKeyDown(e){
+
+    document.onkeydown = function handleKeyDown(e) {
         var key = e.keyCode;
         var newDirection;
-        switch(key){
+        switch (key) {
             case 37:
                 newDirection = "left";
                 break;
@@ -245,4 +273,4 @@ window.onload = function(){
         }
         snakee.setDirection(newDirection);
     };
-}
+};
